21bc3ad26701e116dc4fd3490a51469c0d4adbb6
xzli8 <xzli8@mail.ustc.edu.cn>
2023-11-19 11:52:59 +0800

    add

--- src/main/java/dsa/leetcode/_343integerBreak.java
+++ src/main/java/dsa/leetcode/_343integerBreak.java
@@ -65,12 +65,41 @@ public class _343integerBreak {
      }
 
     /**
+     动态规划
+     定义状态：dp[i]表示数字i拆分后的最大乘积
+     状态转移：dp[i] = max(dp[i-j] * dp[j]), 1 <= j <= i/2
+     将i拆分成j和i-j两个数，取乘积的最大值
+     初始状态：dp[2] = 1, dp[3] = 2
+
+     时间复杂度：O(N^2)
+     空间复杂度：O(N)
+     */
+    public int integerBreak3(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            int max = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                max = Math.max(max, dp[j] * dp[i - j]);
+            }
+            dp[i] = max;
+        }
+        return dp[n];
+    }
+
+
+    /**
      数学：拆成尽可能多的3，余数为1时用4，余数为2时用2
      参考题解：https://leetcode.cn/problems/integer-break/solutions/29098/343-zheng-shu-chai-fen-tan-xin-by-jyd/
      时间复杂度：O(1)
      空间复杂度：O(1)
      */
-    public int integerBreak3(int n) {
+    public int integerBreak4(int n) {
         if (n <= 3) {
             return n - 1;
         }
