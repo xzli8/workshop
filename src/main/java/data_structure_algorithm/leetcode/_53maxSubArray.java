c1507d42026799b191b79543eaf1beb6f922e155
xzli8 <xzli8@mail.ustc.edu.cn>
2023-11-28 09:43:08 +0800

    add 53

---
+++ src/main/java/data_structure_algorithm/leetcode/_53maxSubArray.java
@@ -0,0 +1,64 @@
+package data_structure_algorithm.leetcode;
+
+public class _53maxSubArray {
+
+
+
+    public static class Solution1 {
+
+        /**
+         动态规划
+         定义状态：dp[i]表示以nums[i]结尾的子数组的最大和
+         状态转移：dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+         初始状态：dp[0] = nums[0]
+
+         时间复杂度：O(N)
+         空间复杂度：O(N)
+         */
+         public int maxSubArray(int[] nums) {
+             // 定义状态
+             int n = nums.length;
+             int[] dp = new int[n];
+
+             // 初始状态
+             dp[0] = nums[0];
+
+             // 状态转移
+             int max = dp[0];
+             for (int i = 1; i < n; i++) {
+                 dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+                 max = Math.max(max, dp[i]);
+             }
+             return max;
+         }
+
+    }
+
+
+
+    public static class Solution2 {
+
+        /**
+         空间压缩：dp[i]只与dp[i - 1]有关
+         */
+        public int maxSubArray(int[] nums) {
+            int dp = nums[0];
+            int max = dp;
+            for (int i = 1; i < nums.length; i++) {
+                dp = Math.max(nums[i], dp + nums[i]);
+                max = Math.max(dp, max);
+            }
+            return max;
+        }
+
+    }
+
+
+
+    public static class Solution3 {
+
+    }
+
+
+
+}
