59362fa1c349c071f139fa3b48912bf2be656cd4
xzli8 <xzli8@mail.ustc.edu.cn>
2023-11-28 23:22:11 +0800

    fix 1143

--- src/main/java/data_structure_algorithm/leetcode/_1143longestCommonSubsequence.java
+++ src/main/java/data_structure_algorithm/leetcode/_1143longestCommonSubsequence.java
@@ -1,7 +1,5 @@
 package data_structure_algorithm.leetcode;
 
-import org.junit.Test;
-
 public class _1143longestCommonSubsequence {
 
     public static class Solution1 {
@@ -24,14 +22,14 @@ public class _1143longestCommonSubsequence {
             dp[0][0] = text1.charAt(0) == text2.charAt(0) ? 1 : 0;
             for (int i = 1; i < n1; i++) {
                 if (text1.charAt(i) == text2.charAt(0)) {
-                    dp[i][0] = dp[i - 1][0] + 1;
+                    dp[i][0] = 1;
                 } else {
                     dp[i][0] = dp[i - 1][0];
                 }
             }
             for (int j = 1; j < n2; j++) {
                 if (text2.charAt(j) == text1.charAt(0)) {
-                    dp[0][j] = dp[0][j - 1] + 1;
+                    dp[0][j] = 1;
                 } else {
                     dp[0][j] = dp[0][j - 1];
                 }
@@ -50,33 +48,11 @@ public class _1143longestCommonSubsequence {
             return dp[n1 - 1][n2 - 1];
         }
 
-        @Test
-        public void test() {
-//            System.out.println(longestCommonSubsequence("abcde", "ace"));
-
-            String text1 = "aa";
-            String text2 = "aaa";
-            System.out.println(longestCommonSubsequence(text1, text2));
-            System.out.println(text1.length());
-            System.out.println(text2.length());
-        }
-
     }
 
 
 
     public static class Solution2 {
-
-        @Test
-        public void test() {
-//            System.out.println(longestCommonSubsequence("abcde", "ace"));
-
-            String text1 = "aa";
-            String text2 = "aaa";
-            System.out.println(longestCommonSubsequence(text1, text2));
-        }
-
-
 
         /**
          哨兵简化初始化逻辑：状态数组多定义一行边界值，dp[i][j]表示text1[0, i-1]和text2[0, j-1]的最长公共子序列
