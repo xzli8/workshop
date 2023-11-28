71f6bc485dfd4f538cc84fb0dc437192f0ca274c
xzli8 <xzli8@mail.ustc.edu.cn>
2023-11-28 09:29:19 +0800

    add

--- src/main/java/data_structure_algorithm/leetcode/_1143longestCommonSubsequence.java
+++ src/main/java/data_structure_algorithm/leetcode/_1143longestCommonSubsequence.java
@@ -1,8 +1,54 @@
 package data_structure_algorithm.leetcode;
 
+import org.junit.Test;
+
 public class _1143longestCommonSubsequence {
 
     public static class Solution1 {
+
+        /**
+         动态规划
+         定义状态：dp[i][j]表示text1[0...i]和text2[0...j]最长公共子序列
+         状态转移：dp[i][j] = text1[i] == text2[j] ? dp[i-1][j-1] + 1 : max(dp[i-1][j], dp[i][j-1])
+         初始状态：dp[i][0] = text1[i] == text2[0] ? 1 : 0, dp[0][j] = text2[j] == text1[i] ? 1 : 0
+
+         时间复杂度：O(M * N)
+         空间复杂度：O(M * N)
+         */
+        public int longestCommonSubsequence(String text1, String text2) {
+            // 定义状态
+            int n1 = text1.length(), n2 = text2.length();
+            int[][] dp = new int[n1][n2];
+
+            // 初始状态
+            for (int i = 0; i < n1; i++) {
+                if (text1.charAt(i) == text2.charAt(0)) {
+                    dp[i][0] = 1;
+                }
+            }
+            for (int j = 0; j < n2; j++) {
+                if (text2.charAt(j) == text1.charAt(0)) {
+                    dp[0][j] = 1;
+                }
+            }
+
+            // 状态转移(i, j循环的先后次序无所谓)
+            for (int i = 1; i < n1; i++) {
+                for (int j = 1; j < n2; j++) {
+                    if (text1.charAt(i) == text2.charAt(j)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            return dp[n1 - 1][n2 - 1];
+        }
+
+        @Test
+        public void test() {
+            System.out.println(longestCommonSubsequence("abcde", "ace"));
+        }
 
     }
 
