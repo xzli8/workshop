4fa309dc9a9da65d3a2ff3571559ed45b9caadce
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-03 12:14:57 +0800

    add

--- src/main/java/data_structure_algorithm/leetcode/_46permute.java
+++ src/main/java/data_structure_algorithm/leetcode/_46permute.java
@@ -1,37 +1,51 @@
 package data_structure_algorithm.leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class _46permute {
 
     public static class Solution1 {
 
+        /**
+         回溯
+         分析：不同与“子集/组合”，排列与顺序有关，相同的元素不同的顺序是不同的排列
+         思路：不能像之前一样用"start"来去重，而要用一个visited数组来表示元素是否使用过
+
+         时间复杂度：O(N! * N)，backtrace被调用次数为排列数A(N, N) = N!，每种结果需要O(N)的时间枚举
+         空间复杂度：O(N)，递归深度为N，临时数组大小为N
+         */
         public List<List<Integer>> permute(int[] nums) {
-            backtrace(nums, 0);
+            backtrace(nums, new boolean[nums.length], new LinkedList<>());
             return res;
         }
 
         private final List<List<Integer>> res = new ArrayList<>();
-        private void backtrace(int[] nums, int k) {
-            if (k == nums.length) {
-                List<Integer> tmp = new ArrayList<>(nums.length);
-                for (int i = 0; i < nums.length; i++) tmp.add(nums[i]);
-                res.add(tmp);
+
+        private void backtrace(int[] nums, boolean[] visited, LinkedList<Integer> path) {
+            // 递归终止条件
+            if (path.size() == nums.length) {
+                res.add(new LinkedList<>(path));
                 return;
             }
 
-            for (int i = k; i < nums.length; i++) {
-                swap(nums, k, i);
-                backtrace(nums, k+1);
-                swap(nums, k, i);
-            }
-        }
+            // 这里每次需要从头开始遍历
+            for (int i = 0; i < nums.length; i++) {
+                // 元素已经被使用过，跳过
+                if (visited[i]) continue;
 
-        private void swap(int[] nums, int i, int j) {
-            int tmp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = tmp;
+                // 使用当前元素
+                visited[i] = true;
+                path.addLast(nums[i]);
+
+                // 进入下一层
+                backtrace(nums, visited, path);
+
+                // 不用当前元素
+                visited[i] = false;
+                path.removeLast();
+            }
         }
 
     }
--- src/main/java/data_structure_algorithm/leetcode/_77combine.java
+++ src/main/java/data_structure_algorithm/leetcode/_77combine.java
@@ -9,24 +9,36 @@ public class _77combine {
 
     public static class Solution1 {
 
-        // 回溯
+        /**
+         回溯(类似题："78.子集")
+         思路：与"78.子集"思路基本相同，唯一不同的是，需要加上递归终止条件
+         时间复杂度：O(C(k, n) * k)，组合数C(k, n)中结果，每种结果K个数需要O(K)的时间复杂度枚举
+         空间复杂度：O(K)，递归栈深度为k，临时数组大小为k
+         */
         public List<List<Integer>> combine(int n, int k) {
             backtrace(n, k, 1, new LinkedList<>());
             return res;
         }
 
-        private final List<List<Integer>> res = new ArrayList<>();
-        private void backtrace(int n, int k, int start, Deque<Integer> path) {
-            if (k == 0) {
-                res.add(new ArrayList<>(path));
+        private List<List<Integer>> res = new ArrayList<>();
+
+        private void backtrace(int n, int k, int start, LinkedList<Integer> path) {
+            // 递归终止条件：找到k个数后就停下来
+            if (path.size() == k) {
+                res.add(new LinkedList<>(path));
                 return;
             }
 
+            // 遍历当前层
             for (int i = start; i <= n; i++) {
-                path.offerLast(i);
-                backtrace(n, k - 1, i + 1, path);
-                path.pollLast();
+                // 当前节点加入
+                path.addLast(i);
+                // 进入下一层(注意"i+1"防止重复选择)
+                backtrace(n, k, i + 1, path);
+                // 回到当前层，当前节点不选择
+                path.removeLast();
             }
+
         }
 
     }
