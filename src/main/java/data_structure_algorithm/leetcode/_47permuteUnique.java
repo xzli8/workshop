513d1b30fdc36608ddd76dee96292361a26edc67
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-07 21:58:04 +0800

    add 47

--- src/main/java/data_structure_algorithm/leetcode/_47permuteUnique.java
+++ src/main/java/data_structure_algorithm/leetcode/_47permuteUnique.java
@@ -1,48 +1,43 @@
 package data_structure_algorithm.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class _47permuteUnique {
 
     public static class Solution1 {
 
-        // 回溯
+        /**
+         回溯：在选择时，同一层相同元素的相对顺序要保持一致
+         时间复杂度：O(N! * N)
+         空间复杂度：O(N)
+         */
         public List<List<Integer>> permuteUnique(int[] nums) {
-            dfs(nums, 0);
+            Arrays.sort(nums);
+            backtrace(nums, new boolean[nums.length], new LinkedList<>());
             return res;
         }
 
-        List<List<Integer>> res = new ArrayList<>();
+        private final List<List<Integer>> res = new ArrayList<>();
 
-        private void dfs(int[] nums, int k) {
-            int n = nums.length;
-            if (k == n-1) {
-                List<Integer> tmp = new ArrayList<>(n);
-                for (int i = 0; i < n; i++) tmp.add(nums[i]);
-                res.add(tmp);
+        private void backtrace(int[] nums, boolean[] visited, LinkedList<Integer> path) {
+            if (path.size() == nums.length) {
+                res.add(new LinkedList<>(path));
                 return;
             }
 
-            for (int i = k; i < n; i++) {
-                if (!canSwap(nums, i, k)) continue;
-                swap(nums, k, i);
-                dfs(nums, k+1);
-                swap(nums, k, i);
+            for (int i = 0; i < nums.length; i++) {
+                if (visited[i]) continue;
+                if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;  // 去重
+                // if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1]) continue;  // 也能去重，但效率更低
+                visited[i] = true;
+                path.addLast(nums[i]);
+                backtrace(nums, visited, path);
+                path.removeLast();
+                visited[i] = false;
             }
-        }
-
-        private void swap(int[] nums, int i, int j) {
-            int tmp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = tmp;
-        }
-
-        private boolean canSwap(int[] nums, int i, int k) {
-            for (int j = k; j < i; j++) {
-                if (nums[j] == nums[i]) return false;
-            }
-            return true;
         }
 
     }
