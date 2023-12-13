ae8f593290b9b82dfbbda1359d320f9adba8f8ad
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-13 22:02:30 +0800

    add

--- src/main/java/data_structure_algorithm/leetcode/_283moveZeroes.java
+++ src/main/java/data_structure_algorithm/leetcode/_283moveZeroes.java
@@ -5,6 +5,7 @@ public class _283moveZeroes {
     public static class Solution1 {
 
         /**
+         /**
          双指针：慢指针指向第一个未被处理的元素，快指针指向当前遍历元素
          时间复杂度：O(N)
          空间复杂度：O(1)
@@ -13,19 +14,15 @@ public class _283moveZeroes {
             int slow = 0, fast = 0;
             while (fast < nums.length) {
                 if (nums[fast] != 0) {
-                    if (fast != slow) {
-                        swap(nums, slow, fast);
-                    }
-                    slow++;
+                    nums[slow++] = nums[fast];
                 }
                 fast++;
             }
-        }
 
-        private void swap(int[] nums, int i, int j) {
-            int tmp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = tmp;
+            // 尾部元素赋值为0
+            while (slow < nums.length) {
+                nums[slow++] = 0;
+            }
         }
 
     }
