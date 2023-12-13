6fa3143171b222001a83243eae401c5ca22e930d
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-13 19:09:25 +0800

    add 234

--- src/main/java/data_structure_algorithm/leetcode/_234isPalindrome.java
+++ src/main/java/data_structure_algorithm/leetcode/_234isPalindrome.java
@@ -41,4 +41,48 @@ public class _234isPalindrome {
 
     }
 
+
+
+    public static class Solution2 {
+
+        /**
+         双指针：快慢指针找中点，然后反转前半部分，再逐一比较
+         时间复杂度：O(N)
+         空间复杂度：O(1)
+         */
+        public boolean isPalindrome(ListNode head) {
+            if (head == null || head.next == null) {
+                return true;
+            }
+
+            // 快慢指针找中点
+            ListNode fast = head, slow = head, prev = null;
+            while (fast != null && fast.next != null) {
+                prev = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            prev.next = null;
+
+            // 反转前半部分
+            prev = null;
+            while (head != null) {
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+
+            // 逐一比较
+            if (fast != null) slow = slow.next;     // 链表节点是奇数个，跳过中间节点
+            while (prev != null) {
+                if (prev.val != slow.val) return false;
+                prev = prev.next;
+                slow = slow.next;
+            }
+            return true;
+        }
+
+    }
+
 }
