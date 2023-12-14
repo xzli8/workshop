926f72dd0bab82c51f915fa20f87c01bcc011f2d
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-14 19:25:40 +0800

    add

--- src/main/java/data_structure_algorithm/leetcode/_1twoSum.java
+++ src/main/java/data_structure_algorithm/leetcode/_1twoSum.java
@@ -7,21 +7,69 @@ public class _1twoSum {
     public static class Solution1 {
 
         /**
-         哈希表
-         时间复杂度：O(N)
-         空间复杂度：O(N)
+         暴力搜索
+         时间复杂度：O(N^2)
+         空间复杂度：O(1)
          */
         public int[] twoSum(int[] nums, int target) {
-            HashMap<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                if (map.containsKey(target - nums[i])) {
-                    return new int[] {i, map.get(target - nums[i])};
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[] {i, j};
+                    }
                 }
-                map.put(nums[i], i);
             }
-            return new int[0];
+            return new int[] {};
         }
 
     }
+
+
+
+    public static class Solution2 {
+
+        /**
+         哈希表：构建从元素到下标的映射，再找元素对
+         时间复杂度：O(N)
+         空间复杂度：O(N)
+         */
+         public int[] twoSum(int[] nums, int target) {
+             HashMap<Integer, Integer> num2Index = new HashMap<>();
+             for (int i = 0; i < nums.length; i++) {
+                 num2Index.put(nums[i], i);
+             }
+             for (int i = 0; i < nums.length; i++) {
+                 if (num2Index.containsKey(target - nums[i]) && num2Index.get(target - nums[i]) != i) {
+                     return new int[] {i, num2Index.get(target - nums[i])};
+                 }
+             }
+             return new int[] {};
+         }
+
+    }
+
+
+
+    public static class Solution3 {
+
+        /**
+         哈希表：构建从元素到下标的映射，同时找元素对
+         时间复杂度：O(N)
+         空间复杂度：O(N)
+         */
+         public int[] twoSum(int[] nums, int target) {
+             HashMap<Integer, Integer> num2Index = new HashMap<>();
+             for (int i = 0; i < nums.length; i++) {
+                 if (num2Index.containsKey(target - nums[i])) {
+                     return new int[] {i, num2Index.get(target - nums[i])};
+                 }
+                 num2Index.put(nums[i], i);
+             }
+             return new int[] {};
+         }
+
+    }
+
+
 
 }
