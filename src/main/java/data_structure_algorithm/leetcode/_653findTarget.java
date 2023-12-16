4299b151ff345b7763a0fd7b7bc66899902b0693
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-16 16:33:22 +0800

    add

--- src/main/java/data_structure_algorithm/leetcode/_653findTarget.java
+++ src/main/java/data_structure_algorithm/leetcode/_653findTarget.java
@@ -2,10 +2,7 @@ package data_structure_algorithm.leetcode;
 
 import data_structure_algorithm.domain.TreeNode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class _653findTarget {
 
@@ -65,6 +62,80 @@ public class _653findTarget {
          }
 
     }
+
+
+
+    public static class Solution3 {
+
+        /**
+         dfs遍历+搜索：(类似题：“1214.查找两棵二叉搜索树之和”)
+         思路：枚举其中一个节点，在BST中搜索另一个节点，需要注意搜索到枚举节点本身的情况
+         时间复杂度：O(NlogN)
+         空间复杂度：O(logN)，取决于二叉树的深度
+         */
+         public boolean findTarget(TreeNode root, int k) {
+             return doFindTarget(root, root, k);
+         }
+
+         private boolean doFindTarget(TreeNode root1, TreeNode root2, int target) {
+             if (root1 == null) return false;
+             // 当枚举节点的值等于target时，命中了该节点本身，不符合要求，需要排除
+             if (root1.val != target - root1.val && find(root2, target - root1.val)) return true;
+             return doFindTarget(root1.left, root2, target) || doFindTarget(root1.right, root2, target);
+         }
+
+         private boolean find(TreeNode root, int target) {
+             if (root == null) return false;
+             if (root.val == target) {
+                 return true;
+             } else if (root.val < target) {
+                 return find(root.right, target);
+             } else {
+                 return find(root.left, target);
+             }
+         }
+
+    }
+
+
+
+    public static class Solution4 {
+
+        /**
+         bfs遍历+搜索：(类似题：“1214.查找两棵二叉搜索树之和”)
+         思路：枚举其中一个节点，在BST中搜索另一个节点，需要注意搜索到枚举节点本身的情况
+         时间复杂度：O(NlogN)
+         空间复杂度：O(N)，取决于二叉树的最大宽度
+         */
+        public boolean findTarget(TreeNode root, int k) {
+            if (root == null) return false;
+
+            // bfs遍历
+            Queue<TreeNode> q = new ArrayDeque<>();
+            q.offer(root);
+            while (!q.isEmpty()) {
+                TreeNode cur = q.poll();
+                if (cur.val != k - cur.val && find(root, k - cur.val)) return true;
+                if (cur.left != null) q.offer(cur.left);
+                if (cur.right != null) q.offer(cur.right);
+            }
+            return false;
+        }
+
+        private boolean find(TreeNode root, int target) {
+            if (root == null) return false;
+            if (root.val == target) {
+                return true;
+            } else if (root.val < target) {
+                return find(root.right, target);
+            } else {
+                return find(root.left, target);
+            }
+        }
+
+    }
+
+
 
 
 }
