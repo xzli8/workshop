c0b1d221d7653d31b51fbecec7ff677a4b1c043c
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-17 12:06:50 +0800

    add

---
+++ src/main/java/data_structure_algorithm/leetcode/_478generateRandomPointInACircle.java
@@ -0,0 +1,27 @@
+package data_structure_algorithm.leetcode;
+
+public class _478generateRandomPointInACircle {
+
+
+    /**
+     * Your Solution object will be instantiated and called as such:
+     * Solution obj = new Solution(radius, x_center, y_center);
+     * double[] param_1 = obj.randPoint();
+     */
+    public static class Solution1 {
+
+        class Solution {
+
+            public Solution(double radius, double x_center, double y_center) {
+
+            }
+
+            public double[] randPoint() {
+                return null;
+            }
+        }
+
+
+    }
+
+}
---
+++ src/main/java/data_structure_algorithm/leetcode/_497randomPointInNonOverlappingRectangles.java
@@ -0,0 +1,27 @@
+package data_structure_algorithm.leetcode;
+
+public class _497randomPointInNonOverlappingRectangles {
+
+    /**
+     * Your Solution object will be instantiated and called as such:
+     * Solution obj = new Solution(rects);
+     * int[] param_1 = obj.pick();
+     */
+
+    public static class Solution1 {
+
+        class Solution {
+
+            public Solution(int[][] rects) {
+
+            }
+
+            public int[] pick() {
+                return null;
+            }
+
+        }
+
+    }
+
+}
