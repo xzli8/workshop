9dcba71b65188e94436aa85ca73dfb38141430ef
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-17 11:29:49 +0800

    add

---
+++ src/main/java/data_structure_algorithm/leetcode/_1198smallestCommonElement.java
@@ -0,0 +1,14 @@
+package data_structure_algorithm.leetcode;
+
+public class _1198smallestCommonElement {
+
+    /**
+     *  参考题解：
+     *      https://www.cnblogs.com/seyjs/p/11596008.html
+     */
+
+    public static class Solution1 {
+
+    }
+
+}
---
+++ src/main/java/data_structure_algorithm/leetcode/_1213arraysIntersection.java
@@ -0,0 +1,34 @@
+package data_structure_algorithm.leetcode;
+
+public class _1213arraysIntersection {
+
+    /**
+     * LeetCode 1213. 三个有序数组的交集(Intersection of Three Sorted Arrays)
+     *
+     *      题目描述：给出三个均为 严格递增排列 的整数数组 arr1，arr2 和 arr3。
+     *              返回一个由 仅 在这三个数组中 同时出现 的整数所构成的有序数组。
+     *      样例：
+     *          输入: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
+     *          输出: [1,5]
+     *          解释: 只有 1 和 5 同时在这三个数组中出现。
+     *      限制：
+     *          1 <= arr1.length, arr2.length, arr3.length <= 1000
+     *          1 <= arr1[i], arr2[i], arr3[i] <= 2000
+     */
+
+    /**
+     *  参考题解：
+     *      https://www.acwing.com/solution/LeetCode/content/5028/
+     *      https://www.cnblogs.com/Dylan-Java-NYC/p/12047372.html
+     *      https://www.cnblogs.com/seyjs/p/11629272.html
+     *
+     *      类似题：1198.找出所有公共行的最小元素
+     *      https://www.cnblogs.com/seyjs/p/11596008.html
+     */
+
+    public static class Solution1 {
+
+
+    }
+
+}
