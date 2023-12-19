82d5cfbc70a998254eff62947d3b738a9c2498f6
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-19 22:16:12 +0800

    add

--- src/main/java/data_structure_algorithm/leetcode/_1991findMiddleIndex.java
+++ src/main/java/data_structure_algorithm/leetcode/_1991findMiddleIndex.java
@@ -1,11 +1,57 @@
 package data_structure_algorithm.leetcode;
 
+import java.util.Arrays;
+
 public class _1991findMiddleIndex {
 
     public static class Solution1 {
 
+        /**
+         前缀和
+         时间复杂度：O(N)
+         空间复杂度：O(1)
+         */
         public int findMiddleIndex(int[] nums) {
-            return 0;
+            // 计算数组左右元素的和
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+
+            // 计算数组的前缀和
+            int preSum = 0;
+            sum -= nums[0];
+            if (preSum == sum) return 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                preSum += nums[i];
+                sum -= nums[i + 1];
+                if (preSum == sum) {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+
+    }
+
+
+
+    public static class Solution2 {
+
+        /**
+         前缀和：左求和 * 2 + 中间索引值 = total
+         时间复杂度：O(N)
+         空间复杂度：O(1)
+         */
+        public int findMiddleIndex(int[] nums) {
+            int total = Arrays.stream(nums).sum(), sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (sum * 2 + nums[i] == total) {
+                    return i;
+                }
+                sum += nums[i];
+            }
+            return -1;
         }
 
     }
--- src/main/java/data_structure_algorithm/leetcode/_724pivotIndex.java
+++ src/main/java/data_structure_algorithm/leetcode/_724pivotIndex.java
@@ -1,11 +1,60 @@
 package data_structure_algorithm.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class _724pivotIndex {
 
     public static class Solution1 {
 
+        /**
+         前缀和
+         时间复杂度：O(N)
+         空间复杂度：O(1)
+         */
         public int pivotIndex(int[] nums) {
-            return 0;
+            // 计算数组所有元素和
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+
+            // 计算前缀和，同时判断是否能二分数组
+            int preSum = 0;
+            sum -= nums[0];
+            if (preSum == sum) return 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                preSum += nums[i];
+                sum -= nums[i + 1];
+                if (preSum == sum) {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+
+    }
+
+
+
+    public static class Solution2 {
+
+        /**
+         前缀和: 左求和 * 2 + 中心索引值 = total
+         时间复杂度：O(N)
+         空间复杂度：O(1)
+         */
+        public int pivotIndex(int[] nums) {
+            int total = Arrays.stream(nums).sum(), sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (sum * 2 + nums[i] == total) {
+                    return i;
+                }
+                sum += nums[i];
+            }
+            return -1;
         }
 
     }
