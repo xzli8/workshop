5dcefe7abad6f2383a626aefd1080adaff31753f
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-23 10:34:38 +0800

    add 862

--- src/main/java/data_structure_algorithm/leetcode/_862shortestSubarray.java
+++ src/main/java/data_structure_algorithm/leetcode/_862shortestSubarray.java
@@ -1,11 +1,44 @@
 package data_structure_algorithm.leetcode;
 
+import javafx.util.Pair;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class _862shortestSubarray {
 
     public static class Solution1 {
+
+        /**
+         前缀和 + 暴力搜素(timeout)
+         时间复杂度：O(N^2)
+         空间复杂度：O(N)
+         */
+         public int shortestSubarray(int[] nums, int k) {
+             // 计算前缀和
+             int n = nums.length;
+             long[] preSum = new long[n + 1];
+             for (int i = 1; i <= n; i++) {
+                 preSum[i] = preSum[i - 1] + nums[i - 1];
+             }
+
+             // 遍历前缀和区间，求区间和至少为k的最短非空子数组
+             int minLen = Integer.MAX_VALUE;
+             for (int i = 0; i <= n; i++) {
+                 for (int j = i + 1; j <= n; j++) {
+                     if (preSum[j] - preSum[i] >= k) {
+                         minLen = Math.min(minLen, j - i);
+                     }
+                 }
+             }
+             return minLen == Integer.MAX_VALUE ? -1 : minLen;
+         }
+
+    }
+
+
+
+    public static class Solution2 {
 
         /**
          前缀和 + 单调队列
@@ -38,6 +71,45 @@ public class _862shortestSubarray {
                 q.addLast(i);
             }
             return res > n ? -1 : res;
+        }
+
+    }
+
+
+
+    public static class Solution3 {
+
+        /**
+         前缀和 + 单调队列：在遍历数组元素的同时计算前缀和，需要在单调队列中多记录前缀和对应的下标
+         时间复杂度：O(N)
+         空间复杂度：O(N)
+         */
+        public int shortestSubarray(int[] nums, int k) {
+            // 初始化
+            int n = nums.length, minLen = Integer.MAX_VALUE;
+            long preSum = 0;
+            Deque<Pair<Long, Integer>> dq = new ArrayDeque<>();
+            dq.offerLast(new Pair<>(0L, -1));
+
+            // 一边遍历一边计算前缀和
+            for (int i = 0; i < n; i++) {
+                // 计算前缀和
+                preSum += nums[i];
+
+                // 满足条件时计算前缀和并且删除多余元素
+                while (!dq.isEmpty() && preSum - dq.peekFirst().getKey() >= k) {
+                    minLen = Math.min(minLen, i - dq.pollFirst().getValue());
+                }
+
+                // 维持队列递增
+                while (!dq.isEmpty() && dq.peekLast().getKey() >= preSum) {
+                    dq.pollLast();
+                }
+
+                // 前缀和加入队列
+                dq.offerLast(new Pair<>(preSum, i));
+            }
+            return minLen == Integer.MAX_VALUE ? -1 : minLen;
         }
 
     }
