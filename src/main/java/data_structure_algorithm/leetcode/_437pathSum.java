a5527a29ea703893e3ae8d3b4e3069ba27aa2e94
xzli8 <xzli8@mail.ustc.edu.cn>
2023-12-25 09:01:12 +0800

    add 437

--- src/main/java/data_structure_algorithm/leetcode/_437pathSum.java
+++ src/main/java/data_structure_algorithm/leetcode/_437pathSum.java
@@ -7,52 +7,74 @@ import java.util.Map;
 
 public class _437pathSum {
 
-    public static class Solution1 {
+    public class Solution1 {
 
         /**
-         前缀和：最开始的思路是构建一颗新的树，每个节点有指向父节点的指针和根节点到当前节点的路径和，然后在这棵树上递归。
-         上面的做法能找出具体的路径，但题目只要求找出路径的数目，所以不需要记录这么多信息。
+         DFS：以二叉树上的每个节点为根节点，向下遍历，计算路径和
+         分析：如果有需要的话，可以遍历求出所有路径，但该方法时间复杂度较高。
+         时间复杂度：O(N^2)
+         空间复杂度：O(N)
+         */
+         public int pathSum(TreeNode root, int targetSum) {
+             if (root == null) return 0;
+             return pathSum(root.left, targetSum) + pathSum(root.right, targetSum) + rootSum(root, targetSum);
+         }
+
+         // 计算以root为根节点的二叉树中和为target的数量(这里入参target要用long，否则会溢出)
+         private int rootSum(TreeNode root, long targetSum) {
+             if (root == null) return 0;
+             targetSum -= root.val;
+             return rootSum(root.left, targetSum) + rootSum(root.right, targetSum) + (targetSum == 0 ? 1 : 0);
+         }
+
+    }
+
+
+
+    public class Solution2 {
+
+        /**
+         DFS + 前缀和：只需要计算个数，不需要计算具体路径，所以可以用前缀和简化
+         时间复杂度：O(N)
+         空间复杂度：O(N)
          */
         public int pathSum(TreeNode root, int targetSum) {
             // key是从根节点到当前节点的前缀和，value为前缀和对应的数量
-            Map<Long, Integer> preSumCount = new HashMap<>();
+            Map<Long, Integer> preSum2Count = new HashMap<>();
 
             // 前缀和为0，只有一条路径(初始化条件)
-            preSumCount.put(0L, 1);
+            preSum2Count.put(0L, 1);
 
-            // 递归遍历
-            return backtrace(root, 0, targetSum, preSumCount);
+            // 遍历以root为根节点的树
+            return dfs(root, 0, targetSum, preSum2Count);
         }
 
-        /**
-         回溯：cur表示当前节点，curSum表示当前节点的前缀和
-         */
-        private int backtrace(TreeNode cur, long curSum, int targetSum, Map<Long, Integer> preSumCount) {
+        // 计算当前节点的前缀和及其对应数量
+        private int dfs(TreeNode cur, long preSum, int targetSum, Map<Long, Integer> preSum2Count) {
             // 递归终止条件
             if (cur == null) {
                 return 0;
             }
 
             // 计算当前节点的前缀和
-            curSum += cur.val;
+            preSum += cur.val;
 
             // 计算截止当前节点满足targetSum的数量
-            int count = preSumCount.getOrDefault(curSum - targetSum, 0);
+            int count = preSum2Count.getOrDefault(preSum - targetSum, 0);
 
             // 更新前缀和的数量
-            preSumCount.put(curSum, preSumCount.getOrDefault(curSum, 0) + 1);
+            preSum2Count.put(preSum, preSum2Count.getOrDefault(preSum, 0) + 1);
 
             // 进入下一层
-            count += backtrace(cur.left, curSum, targetSum, preSumCount);
-            count += backtrace(cur.right, curSum, targetSum, preSumCount);
+            count += dfs(cur.left, preSum, targetSum, preSum2Count);
+            count += dfs(cur.right, preSum, targetSum, preSum2Count);
 
-            // 回到本层，恢复状态，去除当前节点对应的前缀和数量
-            preSumCount.put(curSum, preSumCount.get(curSum) - 1);
+            // 回到本层，恢复状态，去除当前节点对应的前缀和数量(回溯)
+            preSum2Count.put(preSum, preSum2Count.get(preSum) - 1);
 
             // 返回结果
             return count;
         }
-
 
     }
 
