e5be4b0dab77523450461433871c1b38b186ac45
xzli8 <xzli8@mail.ustc.edu.cn>
2024-01-14 23:14:53 +0800

    add

--- src/main/java/data_structure_algorithm/leetcode/_207canFinish.java
+++ src/main/java/data_structure_algorithm/leetcode/_207canFinish.java
@@ -1,17 +1,10 @@
 package data_structure_algorithm.leetcode;
 
-import org.junit.Test;
-
 import java.util.*;
 
 public class _207canFinish {
 
     public static class Solution1 {
-
-        @Test
-        public void test() {
-            System.out.println(canFinish(20, new int[][] {{0, 10}, {3, 18}, {5, 5}, {6, 11}, {11, 14}, {13, 1}, {15, 1}, {17, 4}}));
-        }
 
         /**
          BFS拓扑排序
@@ -22,9 +15,7 @@ public class _207canFinish {
             // 构建邻接表与入度
             int[] indeg = new int[numCourses];
             List<List<Integer>> g = new ArrayList<>();
-            for (int i = 0; i < numCourses; i++) {
-                g.add(new ArrayList<>());
-            }
+            for (int i = 0; i < numCourses; i++) g.add(new ArrayList<>());
             for (int i = 0; i < prerequisites.length; i++) {
                 g.get(prerequisites[i][1]).add(prerequisites[i][0]);
                 indeg[prerequisites[i][0]]++;
@@ -60,11 +51,6 @@ public class _207canFinish {
 
     public static class Solution2 {
 
-        @Test
-        public void test() {
-            System.out.println(canFinish(20, new int[][] {{0, 10}, {3, 18}, {5, 5}, {6, 11}, {11, 14}, {13, 1}, {15, 1}, {17, 4}}));
-        }
-
         /**
          拓扑排序（邻接表）
          时间复杂度：O(N)
@@ -74,7 +60,8 @@ public class _207canFinish {
             // 构建邻接表与入度
             int[] indeg = new int[numCourses];
             List<Integer>[] g = new List[numCourses];
-            Arrays.fill(g, new ArrayList<>());
+            for (int i = 0; i < numCourses; i++) g[i] = new ArrayList<>();
+            // Arrays.fill(g, new ArrayList<>());    // 这样写填充的是同一个ArrayList对象，有问题
             for (int i = 0; i < prerequisites.length; i++) {
                 g[prerequisites[i][1]].add(prerequisites[i][0]);
                 indeg[prerequisites[i][0]]++;
@@ -104,21 +91,6 @@ public class _207canFinish {
             return visited == numCourses;
         }
 
-    }
-
-    @Test
-    public void test() {
-        int n = 3;
-        List<Integer>[] lists = new List[n];
-        Arrays.fill(lists, new ArrayList<>());
-        for (int i = 0; i < n; i++) {
-            lists[i].add(i);
-        }
-        for (int i = 0; i < n; i++) {
-            for (int num : lists[i]) {
-                System.out.println(num);
-            }
-        }
     }
 
 }
